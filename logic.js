2e85bcfc9a0c5c65a755ad30095cd792d1338b30
Geony Ayyad <geonyayyad@Geonys-MacBook-Pro.local>
2017-12-22 11:52:23 -0500

    new changes withevent.map.Object functionality

--- logic.js
+++ logic.js
@@ -18,3 +18,15 @@ var map = AmCharts.makeChart( "chartdiv", {
     "position": "bottom-right"
   }
 } );
+
+
+map.addListener("clickMapObject", function(event) {
+  console.log(event);
+  console.log(event.mapObject.id);
+  console.log(event.mapObject.title)
+  document.getElementById("info").innerHTML = 'Clicked ID: ' 
+  + event.mapObject.id + ' (' + event.mapObject.title + ')';
+});
+
+//grab value of EN title 
+// https://www.amcharts.com/kbase/?s=events&kbc=
