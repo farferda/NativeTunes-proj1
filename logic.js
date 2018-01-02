ead1f5e3bb2ca2c9499b0b0de350d3183855aef7
Geony Ayyad <geonyayyad@Geonys-MacBook-Pro.local>
2018-01-02 11:56:27 -0500

    updates with API

--- logic.js
+++ logic.js
@@ -28,7 +28,20 @@ map.addListener("clickMapObject", function(event) {
   + 
   // event.mapObject.id + 
    ' ' + event.mapObject.title + '';
+
+   $.ajax({
+      url: "https://api.spotify.com/v1/browse/new-releases?country=" + event.mapObject.id,
+      method: "GET", 
+      headers: {
+        Authorization: "Bearer BQCvUYCb07cCQ3802EWtIfntY5YXHXSfMpPOl7_VNo4R1tNbkkH-KlhVsHq63BTOTmPiQ-oNOTOeGnz7nsv8hFe2uLi1r01FVLvTYRSkwtRrfhnfo7frBCznPH3FYQKF_LuhnStUVQs"
+      }
+    }).done(function(response) {
+      console.log(response)
+
+    })
+
 });
 
 //grab value of EN title 
 // https://www.amcharts.com/kbase/?s=events&kbc=
+
