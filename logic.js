54fe4381a9797cb3f9e395def06c1aca930063a3
Geony Ayyad <geonyayyad@Geonys-MacBook-Pro.local>
2018-01-03 10:48:03 -0500

    new changes January 2nd

--- logic.js
+++ logic.js
@@ -10,7 +10,7 @@ var map = AmCharts.makeChart( "chartdiv", {
   },
   "areasSettings": {
     "autoZoom": true,
-    "selectedColor": "#CC0000"
+    "selectedColor": "#f1fbff"
   },
   "smallMap": {},
   "export": {
@@ -24,9 +24,24 @@ map.addListener("clickMapObject", function(event) {
   console.log(event);
   console.log(event.mapObject.id);
   console.log(event.mapObject.title)
-  document.getElementById("info").innerHTML = 'Clicked ID: ' 
-  + event.mapObject.id + ' (' + event.mapObject.title + ')';
+  document.getElementById("info").innerHTML = 'You Selected: ' 
+  + 
+  // event.mapObject.id + 
+   ' ' + event.mapObject.title + '';
+
+   $.ajax({
+      url: "https://api.spotify.com/v1/browse/new-releases?country=" + event.mapObject.id,
+      method: "GET", 
+      headers: {
+        Authorization: "Bearer BQCvUYCb07cCQ3802EWtIfntY5YXHXSfMpPOl7_VNo4R1tNbkkH-KlhVsHq63BTOTmPiQ-oNOTOeGnz7nsv8hFe2uLi1r01FVLvTYRSkwtRrfhnfo7frBCznPH3FYQKF_LuhnStUVQs"
+      }
+    }).done(function(response) {
+      console.log(response)
+
+    })
+
 });
 
 //grab value of EN title 
 // https://www.amcharts.com/kbase/?s=events&kbc=
+
